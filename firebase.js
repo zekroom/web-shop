124a7943f0597a9217167be36b2cf322dade1126
Trung <trunglt@reactplus.com>
2021-04-21 08:28:56 +0700

    Firt push

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: "AIzaSyBbwXGUbv3TjZiY1lKzXf5MIJpdg8kvS4Y",
+    authDomain: "product-app-7fb6d.firebaseapp.com",
+    databaseURL: "https://product-app-7fb6d-default-rtdb.firebaseio.com",
+    projectId: "product-app-7fb6d",
+    storageBucket: "product-app-7fb6d.appspot.com",
+    messagingSenderId: "1083132339642",
+    appId: "1:1083132339642:web:b665edc20d2beae93ce983",
+    measurementId: "G-MLPVH26JKY"
+
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  export default firebaseConfig;
+  
+  
---
+++ next.config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+    env: {
+        "BASE_URL": "http://localhost:3000",
+        "MONGODB_URL": "mongodb+srv://zekroom:lt1999hn@cluster0.foeva.mongodb.net/ShopLaptop?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "f%:3FL#XGy7BMbA(ZM]Cxf[e.xEF.txw>RHnv^Dk^AFhs6GhJ8",
+        "REFRESH_TOKEN_SECRET": "t!}@2Q<3=@bqQ*j2N4AhL2=hyM#**MMu<3r4)Wn>.?vA@>}+KF29)2D{Aa}^h<rNK(:Jp*7=MVy&jb^",
+        "PAYPAL_CLIENT_ID": "AX6mxIq152x-RkwgtWDmEx0IHXApfthfFpwn8izhpu9fphAtwS2Epkbgcuczzj_amvDQOoJ4K33kNsNK",
+        "CLOUD_UPDATE_PRESET": "product_stores",
+        "CLOUD_NAME": "productstore",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/productstore/image/upload"
+    },
+    firebase:{
+        apiKey: "AIzaSyBbwXGUbv3TjZiY1lKzXf5MIJpdg8kvS4Y",
+    authDomain: "product-app-7fb6d.firebaseapp.com",
+    databaseURL: "https://product-app-7fb6d-default-rtdb.firebaseio.com",
+    projectId: "product-app-7fb6d",
+    storageBucket: "product-app-7fb6d.appspot.com",
+    messagingSenderId: "1083132339642",
+    appId: "1:1083132339642:web:b665edc20d2beae93ce983",
+    measurementId: "G-MLPVH26JKY"
+    }
+    
+    }
+    
